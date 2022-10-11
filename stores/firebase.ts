ca7a1e83013ed0a33cbe82be01bf41eef82827e2
Maxime Allanic <maxime@allanic.me>
2022-10-11 10:11:25 +0200

    fix env import firebase config

--- stores/firebase.ts
+++ stores/firebase.ts
@@ -15,16 +15,16 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 export const useFirebase = defineStore("firebase", () => {
     // Your web app's Firebase configuration
     const firebaseConfig = {
-        apiKey: import.meta.env.NODE_FIREBASE_API_KEY,
-        authDomain: import.meta.env.NODE_FIREBASE_AUTH_DOMAIN,
-        databaseURL: import.meta.env.NODE_FIREBASE_DATABASE_URL,
-        projectId: import.meta.env.NODE_FIREBASE_PROJECT_ID,
-        storageBucket: import.meta.env.NODE_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.NODE_FIREBASE_MESSAGING_SENDER_ID,
-        appId: import.meta.env.NODE_FIREBASE_APP_ID,
-        measurementId: import.meta.env.NODE_FIREBASE_MEASUREMENT_ID,
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
       };
-
+    console.log(firebaseConfig, import.meta);
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
 
