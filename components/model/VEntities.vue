d381481c92b89f4c545c4db967fa50ce0f04a49e
Florent C <fchaussat@outlook.fr>
2022-10-25 16:29:14 +0200

    feat(add): VEntities base

---
+++ components/model/VEntities.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+export interface VEntitiesProps {
+    modelValue?: any;
+    model?: any;
+    property: string;
+    onlyIds: Boolean;
+    multiple: Boolean;
+}
+const props = defineProps<VEntitiesProps>();
+console.log(props);
+</script>
+
+<template>
+    <VFlex>
+        <label class="label"
+            ><Translate>{{ "." + property + ".label" }}</Translate></label
+        >
+    </VFlex>
+    <TranslateNamespace :path="'.' + property"> </TranslateNamespace>
+</template>
+
+<style lang="scss">
+.label {
+    font-family: var(--font);
+    font-size: var(0.9rem);
+    color: var(--light-text) !important;
+}
+</style>
