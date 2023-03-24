3dae0b7c1c7179212064cdc265689eb163e92d96
Charles BLANCHETON <charles.blancheton@gmail.com>
2023-03-24 13:58:19 +0100

    feat(internal): add VMultipage (#4)

---
+++ components/base/form/VMultipage.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { computed, ref, watch } from "vue";
+import VWizard from "./VWizard.vue";
+
+type MultipageModes = "tabs" | "wizard";
+interface MultipageProps {
+    modelValue: any;
+    steps: string[];
+    mode: MultipageModes;
+}
+
+const props = defineProps<MultipageProps>();
+
+const stepNames = computed(() => props.steps.map((_, i) => `step-${i + 1}`));
+const wizardRef = ref<InstanceType<typeof VWizard> | undefined>(undefined);
+
+const tabs = computed(() => {
+    return props.steps.map((stepName, i) => ({
+        label: stepName,
+        value: (i + 1).toString(),
+    }));
+});
+const tabSelected = ref("1");
+watch(tabSelected, () => {
+    if (wizardRef.value === undefined) return;
+    wizardRef.value.setStep(Number.parseInt(tabSelected.value));
+});
+</script>
+
+<template>
+    <VTabs v-if="props.mode === 'tabs'" v-model="tabSelected" :tabs="tabs"> </VTabs>
+    <VWizard
+        :ref="(ref: any) => (wizardRef = ref)"
+        :model-value="props.modelValue"
+        :steps="steps"
+        :hide-actions="props.mode !== 'wizard'"
+        :hide-steps="props.mode !== 'wizard'"
+        @update:current-step="(currentStep: number) => (tabSelected = currentStep.toString())">
+        <template v-for="stepName in stepNames" #[stepName]="{ field }">
+            <slot :name="stepName" :field="field"></slot>
+        </template>
+    </VWizard>
+</template>
