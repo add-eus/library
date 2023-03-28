d0a0ad1c3b9c0046b75f268f3c96be3697f2472e
Maxime Allanic <maxime@allanic.me>
2023-03-28 15:46:05 +0200

    improve env dev compatibility (#14)
    * fix(internal): env compatibility
    
    * feat: add github workflows

---
+++ default-styles.ts
@@ -0,0 +1,17 @@
+import "simplebar";
+import "nprogress/nprogress.css";
+import "@vueform/multiselect/themes/default.scss";
+import "@vueform/slider/themes/default.scss";
+import "simplebar/dist/simplebar.css";
+import "tiny-slider/src/tiny-slider.scss";
+import "notyf/notyf.min.css";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/dist/svg-arrow.css";
+import "tippy.js/dist/border.css";
+import "tippy.js/dist/backdrop.css";
+import "tippy.js/themes/light.css";
+import "v-tooltip/dist/v-tooltip.css";
+import "floating-vue/dist/style.css";
+import "vue-tel-input/dist/vue-tel-input.css";
+import "v-calendar/dist/style.css";
+import "material-icons/iconfont/material-icons.css";
