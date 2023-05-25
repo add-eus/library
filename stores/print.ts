935d7f985c3542acc48c96c24e2da16a5498ac03
Maxime Allanic <maxime@allanic.me>
2023-05-25 11:26:01 +0200

    Update print.ts

--- stores/print.ts
+++ stores/print.ts
@@ -4,6 +4,8 @@ export function usePrint() {
             const temporaryWindows = window.open("", "print");
             if (!temporaryWindows) throw new Error("Could not open temporary window");
             temporaryWindows.document.write(content);
+            if (typeof TEST_PRINT !== "undefined")
+                return;
             temporaryWindows.document.close();
             temporaryWindows.focus();
             temporaryWindows.print();
