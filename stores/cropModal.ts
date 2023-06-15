c189c3e8d38d3c23c85fc3061a80378b3abfb0a6
Charles Blancheton <charles.blancheton@add.eus>
2023-06-15 15:52:40 +0200

    fix(internal): fix large image upload

--- stores/cropModal.ts
+++ stores/cropModal.ts
@@ -16,6 +16,7 @@ export const useCropModal = () => {
     const { translate } = useTranslate();
     return async (src: string, cropOptions?: CropOptions): Promise<Blob> => {
         return new Promise((resolve, reject) => {
+            let cropping = false;
             const modal = modalManager.createModal(VCropComponent, {
                 title: translate("crop-modal.title").value,
                 props: { src, options: cropOptions },
@@ -26,6 +27,7 @@ export const useCropModal = () => {
                         props: {},
                         events: {
                             click() {
+                                if (cropping) return;
                                 modal.close();
                             },
                         },
@@ -38,14 +40,17 @@ export const useCropModal = () => {
                         },
                         events: {
                             async click() {
-                                const blob = await modal.reference.getResult();
-                                resolve(blob);
+                                if (cropping) return;
+                                cropping = true;
+                                const blobPromise = modal.reference.getResult();
                                 modal.close();
+                                resolve(await blobPromise);
                             },
                         },
                     },
                 ],
                 onClose: () => {
+                    if (cropping) return;
                     reject();
                 },
             });
