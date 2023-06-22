a05df90da80c086d201c2219cd1ee0f9fbe2d6d2
Maxime Allanic <maxime@allanic.me>
2023-06-22 16:24:48 +0200

    chore: update comment in array

--- stores/array.ts
+++ stores/array.ts
@@ -10,6 +10,8 @@ export function useSplittedArray<T>(
         const unRefArray = unref(array);
         const result = [];
         let sizePos = 0;
+
+        // If sizes are less than 1, they are considered as percentage of total length of array
         sizes = sizes.map((size) => (size < 1 ? unRefArray.length * size : size));
         for (
             let i = 0;
