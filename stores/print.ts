65d7afdfa971221a0831c168f02ed255e507a94c
Maxime Allanic <maxime@allanic.me>
2023-07-31 13:59:29 +0200

    feat: add print

--- stores/print.ts
+++ stores/print.ts
@@ -1,15 +1,20 @@
 export function usePrint() {
+    function print(temporaryWindows) {
+        if (typeof TEST_PRINT !== "undefined") return;
+        temporaryWindows.document.close();
+        temporaryWindows.focus();
+        temporaryWindows.print();
+        temporaryWindows.close();
+    }
     return {
         print: (content: string) => {
             const temporaryWindows = window.open("", "print");
             if (!temporaryWindows) throw new Error("Could not open temporary window");
             temporaryWindows.document.write(content);
-            if (typeof TEST_PRINT !== "undefined")
-                return;
-            temporaryWindows.document.close();
-            temporaryWindows.focus();
-            temporaryWindows.print();
-            temporaryWindows.close();
+            return print(temporaryWindows);
+        },
+        printFromURL: (url: string) => {
+            return print(window.open(url, "print"));
         },
     };
 }
