5a5a40c7a9c1b8cdb421410e67833edd4675a777
Maxime Allanic <maxime@allanic.me>
2023-10-18 17:19:11 +0200

    fix: isUpdating

--- stores/firestore/index.ts
+++ stores/firestore/index.ts
@@ -175,8 +175,6 @@ export function useCollection<T extends typeof Entity>(
             );
         }
 
-        entities.isUpdating = true;
-
         let limit = 10;
         if (isRef(options.limit) && typeof options.limit.value === "number")
             limit = options.limit.value;
@@ -184,6 +182,8 @@ export function useCollection<T extends typeof Entity>(
         if (limit === 0) return;
 
         try {
+            entities.isUpdating = true;
+
             await query.next(limit);
             entities.isUpdating = false;
         } catch (err) {
@@ -232,7 +232,10 @@ export function useCollection<T extends typeof Entity>(
             await fetch();
         });
 
-    fetch().catch(() => {});
+    fetch().catch((e) => {
+        // eslint-disable-next-line no-console
+        console.error(e);
+    });
 
     return entities;
 }
