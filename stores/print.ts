e9cdf061b14af8cbd47606e9b93e363c145d2b29
Maxime Allanic <maxime@allanic.me>
2023-10-25 13:33:46 +0200

    fix: wait print modal close

--- stores/print.ts
+++ stores/print.ts
@@ -23,7 +23,15 @@ export function usePrint() {
         iframe.focus();
         if (!iframe.contentWindow)
             throw new Error("Could not get content window from iframe");
+
+        const promise = new Promise((resolve, reject) => {
+            if (iframe.contentWindow !== null)
+                return iframe.contentWindow.addEventListener("afterprint", resolve);
+            reject("Could not get content window from iframe");
+        });
         iframe.contentWindow.print();
+        await promise;
+        iframe.remove();
     }
 
     return {
@@ -41,7 +49,6 @@ export function usePrint() {
             const iframe = createPrintFrame();
             await setUrlPrintFrame(iframe, fileURL);
             await print(iframe);
-            iframe.remove();
         },
         printComponent: async (component: any, props: any) => {
             const printFrame = createPrintFrame();
@@ -60,10 +67,9 @@ export function usePrint() {
                 }
             });
             await promise;
+
             await print(printFrame);
-            await new Promise((resolve) => setTimeout(resolve, 1));
             await destroy(node);
-            printFrame.remove();
         },
     };
 }
