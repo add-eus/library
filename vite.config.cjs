5a3c6fedbca160583c7da6dcb133e3d1dc53eac7
Maxime Allanic <maxime.allanic@addeus.ai>
2024-06-24 14:20:34 +0200

    chore: upgrade capacitor

--- vite.config.cjs
+++ vite.config.cjs
@@ -131,6 +131,11 @@ module.exports.define = function (config = {}) {
                     include: "../../lib/**",
                 },*/
             },
+            define: {
+                __VUE_OPTIONS_API__: true,
+                __VUE_PROD_DEVTOOLS__: false,
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
+            },
             server: {
                 host: true,
                 port: config.port !== undefined ? config.port : 8018,
